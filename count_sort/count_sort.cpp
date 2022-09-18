68f39fce0aa767d3ade8390788ffd3b178a842e9
asavin <a.savin@integrasources.com>
2022-09-19 01:19:02 +0700

    Count sort done

--- count_sort/count_sort.cpp
+++ count_sort/count_sort.cpp
@@ -1,11 +1,18 @@
 #include "count_sort.h"
 
-// Return sorted array. 
-// All elements of array are assumed to be from [min, max] range.
 std::vector<int> count_sort(const std::vector<int> &array, int min, int max) {
-    std::vector<int> result;
-
-    // Your implementation here.
-
+    std::vector<int> result {};
+    std::vector<int> counts (max == min ? 1 : max - min + 1);
+    for (const auto& item: array)
+    {
+        ++counts[item - min];
+    }
+    for (std::size_t i {0}; i < counts.size(); ++i)
+    {
+        while (counts[i]--)
+        {
+            result.push_back(static_cast<int>(i) + min);
+        }
+    }
     return result;
 }
--- count_sort/count_sort_test.cpp
+++ count_sort/count_sort_test.cpp
@@ -1,6 +1,22 @@
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+
 #include "../catch.hpp"
 
 #include "count_sort.h"
+
+std::vector<int> random_sequence(int size, int max)
+{
+    std::default_random_engine generator;
+    std:: uniform_int_distribution<int> distribution(0, max);
+    std::vector<int> result;
+    for (int i = 0; i < size; i++) {
+        result.push_back(distribution(generator));
+    }
+    return result;
+}
 
 TEST_CASE( "Empty array", "[count_sort]" ) {    
     CHECK( count_sort({}, 0, 0) == std::vector<int> {} );
@@ -34,3 +50,72 @@ TEST_CASE( "Array with big range", "[count_sort]" ) {
     CHECK( count_sort({1, 0, 1000000, 0, -1000000}, -2000000, 2000000) == std::vector<int> {-1000000, 0, 0, 1, 1000000} );
 }
 
+TEST_CASE(" Time check ")
+{
+    std::vector<int> data   {random_sequence(1000, 1000)};
+    std::cout << std::fixed;
+
+    auto begin  = std::chrono::high_resolution_clock::now();
+    auto result = count_sort(data, 0 , 1000);
+    auto end    = std::chrono::high_resolution_clock::now();
+    std::cout << "count_sort time (1000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << result.size() << std::endl;
+
+    begin  = std::chrono::high_resolution_clock::now();
+    std::sort(data.begin(), data.end());
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "std::sort time (1000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << data.size() << std::endl;
+
+    data   = random_sequence(10000, 10000);
+    begin  = std::chrono::high_resolution_clock::now();
+    result = count_sort(data, 0 , 10000);
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "count_sort time (10000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << result.size() << std::endl;
+
+    begin  = std::chrono::high_resolution_clock::now();
+    std::sort(data.begin(), data.end());
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "std::sort time (10000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << data.size() << std::endl;
+
+    data   = random_sequence(100000, 100000);
+    begin  = std::chrono::high_resolution_clock::now();
+    result = count_sort(data, 0 , 100000);
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "count_sort time (100000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << result.size() << std::endl;
+
+    begin  = std::chrono::high_resolution_clock::now();
+    std::sort(data.begin(), data.end());
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "std::sort time (100000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << data.size() << std::endl;
+
+    data   = random_sequence(1000000, 1000000);
+    begin  = std::chrono::high_resolution_clock::now();
+    result = count_sort(data, 0 , 1000000);
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "count_sort time (1000000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << result.size() << std::endl;
+
+    begin  = std::chrono::high_resolution_clock::now();
+    std::sort(data.begin(), data.end());
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "std::sort time (1000000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << data.size() << std::endl;
+
+    data   = random_sequence(10000000, 10000000);
+    begin  = std::chrono::high_resolution_clock::now();
+    result = count_sort(data, 0 , 10000000);
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "count_sort time (10000000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << result.size() << std::endl;
+
+    begin  = std::chrono::high_resolution_clock::now();
+    std::sort(data.begin(), data.end());
+    end    = std::chrono::high_resolution_clock::now();
+    std::cout << "std::sort time (10000000): " << std::chrono::duration<double>(end - begin).count() << std::endl;
+    std::cout << "array size: " << data.size() << std::endl;
+}
